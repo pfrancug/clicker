3a6cbaa27e517bcafa15a309ab0356716004bdf7
Piotr Francug <piotr.francug@gmail.com>
2016-07-23 13:52:26 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,77 +1,108 @@
-
-
-
 var money = 0;
-var beginner = 0;
-var intermediate = 0;
-var advanced = 0;
-var legendary = 0;
+var One = 0;
+var Two = 0;
+var Three = 0;
+var Four = 0;
+var Five = 0;
+var Six = 0;
 
 function hackClick(number) {
 	money = money + number;
-	document.getElementById("money").innerHTML = money;
+	$('#money').text("$" + Math.round(money));
 };
 
-function buyBeginner() {
-	var beginnerCost = Math.floor(15 * Math.pow(1.2, beginner));
-	if (money >= beginnerCost) {
-		beginner = beginner + 1;
-		money = money - beginnerCost;
-		document.getElementById('beginner').innerHTML = beginner;
-		document.getElementById('money').innerHTML = money;
+$('.miner:nth-child(1)').click(function() {
+	var costOne = Math.floor(15 * Math.pow(1.15, One));
+	if (money >= costOne) {
+		One = One + 1;
+		money = money - costOne;
+		$('#quantityOne').text(One);
+		$('#money').text("$" + Math.round(money));
+		$('.miner:nth-child(2)').fadeIn(800);
 	};
-	var nextCost = Math.floor(15 * Math.pow(1.2, beginner));
-	document.getElementById('beginnerCost').innerHTML = nextCost;
-};
+	var nextCost = Math.floor(15 * Math.pow(1.15, One));
+	$('#costOne').text("$" + nextCost);
+});
 
-function buyIntermediate() {
-	var intermediateCost = Math.floor(100 * Math.pow(1.4, intermediate));
-	if (money >= intermediateCost) {
-		intermediate = intermediate + 1;
-		money = money - intermediateCost;
-		document.getElementById('intermediate').innerHTML = intermediate;
-		document.getElementById('money').innerHTML = money;
+$('.miner:nth-child(2)').click(function() {
+	var costTwo = Math.floor(100 * Math.pow(1.15, Two));
+	if (money >= costTwo) {
+		Two = Two + 1;
+		money = money - costTwo;
+		$('#quantityTwo').text(Two);
+		$('#money').text("$" + Math.round(money));
+		$('.miner:nth-child(3)').fadeIn(800);
 	};
-	var nextCost = Math.floor(100 * Math.pow(1.4, intermediate));
-	document.getElementById('intermediateCost').innerHTML = nextCost;
-};
+	var nextCost = Math.floor(100 * Math.pow(1.15, Two));
+	$('#costTwo').text("$" + nextCost);
+});
 
-function buyAdvanced() {
-	var advancedCost = Math.floor(1100 * Math.pow(1.6, advanced));
-	if (money >= advancedCost) {
-		advanced = advanced + 1;
-		money = money - advancedCost;
-		document.getElementById('advanced').innerHTML = advanced;
-		document.getElementById('money').innerHTML = money;
+$('.miner:nth-child(3)').click(function() {
+	var costThree = Math.floor(1100 * Math.pow(1.15, Three));
+	if (money >= costThree) {
+		Three = Three + 1;
+		money = money - costThree;
+		$('#quantityThree').text(Three);
+		$('#money').text("$" + Math.round(money));
+		$('.miner:nth-child(4)').fadeIn(800);
 	};
-	var nextCost = Math.floor(1100 * Math.pow(1.6, advanced));
-	document.getElementById('advancedCost').innerHTML = nextCost;
-};
-function buyLegendary() {
-	var legendaryCost = Math.floor(11000 * Math.pow(1.8, legendary));
-	if (money >= legendaryCost) {
-		legendary = legendary + 1;
-		money = money - legendaryCost;
-		document.getElementById('legendary').innerHTML = legendary;
-		document.getElementById('money').innerHTML = money;
-	};
-	var nextCost = Math.floor(11000 * Math.pow(1.8, legendary));
-	document.getElementById('legendaryCost').innerHTML = nextCost;
-};
+	var nextCost = Math.floor(1100 * Math.pow(1.15, Three));
+	$('#costThree').text("$" + nextCost);
+});
 
-function set_cookie ( cookie_name, cookie_value, lifespan_in_days, valid_domain )
-{
-  var domain_string = valid_domain ? ("; domain=" + valid_domain) : '' ;
-  document.cookie = cookie_name + "=" + encodeURIComponent( cookie_value ) +
-      "; max-age=" + 60 * 60 * 24 * lifespan_in_days +
-      "; path=/" + domain_string ;
-}
+$('.miner:nth-child(4)').click(function() {
+	var costFour = Math.floor(12000 * Math.pow(1.15, Four));
+	if (money >= costFour) {
+		Four = Four + 1;
+		money = money - costFour;
+		$('#quantityFour').text(Four);
+		$('#money').text("$" + Math.round(money));
+		$('.miner:nth-child(5)').fadeIn(800);
+	};
+	var nextCost = Math.floor(12000 * Math.pow(1.15, Four));
+	$('#costFour').text("$" + nextCost);
+});
+
+$('.miner:nth-child(5)').click(function() {
+	var costFive = Math.floor(130000 * Math.pow(1.15, Five));
+	if (money >= costFive) {
+		Five = Five + 1;
+		money = money - costFive;
+		$('#quantityFive').text(Five);
+		$('#money').text("$" + Math.round(money));
+		$('.miner:nth-child(6)').fadeIn(800);
+	};
+	var nextCost = Math.floor(130000 * Math.pow(1.15, Five));
+	$('#costFive').text("$" + nextCost);
+});
+
+$('.miner:nth-child(6)').click(function() {
+	var costSix = Math.floor(1400000 * Math.pow(1.15, Six));
+	if (money >= costSix) {
+		Six = Six + 1;
+		money = money - costSix;
+		$('#quantitySix').text(Six);
+		$('#money').text("$" + Math.round(money));
+		$('.miner:nth-child(7)').fadeIn(800);
+	};
+	var nextCost = Math.floor(1400000 * Math.pow(1.15, Six));
+	$('#costSix').text("$" + nextCost);
+});
+
+$('.info').click(function() {
+	$('.info, .buildings, .hack').hide();
+});
+
+$('.buildings').click(function() {
+	$('.miner').show();
+});
 
 window.setInterval(function() {
-	hackClick(beginner);
-	hackClick(intermediate * 1);
-	hackClick(advanced * 15);
-	hackClick(legendary * 50);
-	document.title = "Money: " + money;
-
-}, 1000);
+	hackClick(One * 0.1);
+	hackClick(Two * 0.2);
+	hackClick(Three * 0.8);
+	hackClick(Four * 4.7);
+	hackClick(Five * 26);
+	hackClick(Six * 140);
+	$('#money').text("$" + Math.round(money));
+}, 100);
